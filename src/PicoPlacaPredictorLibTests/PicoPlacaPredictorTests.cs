030994fce5ceca8c2078f6bfa53b4274536b6a7b
Armando <arfonte271088@gmail.com>
2015-11-18 22:32:37 -0500

    initializes repository
    Signed-off-by: Armando <arfonte271088@gmail.com>

---
+++ src/PicoPlacaPredictorLibTests/PicoPlacaPredictorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PicoPlacaPredictorLibTests
+{
+    [TestClass]
+    public class PicoPlacaPredictorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
